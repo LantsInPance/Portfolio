f262d28a284789a7d9d8753d601e8332748ed956
Lance McKee <themakeez@gmail.com>
2026-01-28 09:06:36 -0800

    Set up About Me and Skeeball Challenge pages and stubbed out Joyride Jackpot and Area 50 Fun pages

---
+++ assets/js/observer.js
@@ -0,0 +1,16 @@
+// 1. Select the video
+const video = document.querySelector('video');
+
+// 2. Define the observer
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+}, { threshold: 0.5 }); // Pauses when less than 50% visible
+
+// 3. Start observing
+observer.observe(video);
